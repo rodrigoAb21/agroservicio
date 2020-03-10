fbe9edc9d9d1ecf453078e6de78496e113934dc5
rodrigoDesktop <rodrigo.abasto21@gmail.com>
2020-03-10 01:00:48 -0400

    Update create.blade.php

--- resources/views/vistas/ingresos/create.blade.php
+++ resources/views/vistas/ingresos/create.blade.php
@@ -44,29 +44,58 @@
 
                         </div>
                         <hr>
+
                         <div class="row">
-                            <div class="col-12">
-                                <h4>DETALLE</h4>
-                                <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-                                    <div class="form-group">
-                                        <label>Insumo</label>
-                                        <select class="form-control" name="insumo_id">
-                                            @foreach($insumos as $insumo)
-                                                <option value="{{$insumo->id}}">{{$insumo->nombre}}</option>
-                                            @endforeach
-                                        </select>
-                                    </div>
+                            <div class="col-lg-6 col-md-12 col-sm-12 col-xs-12">
+                                <div class="form-group">
+                                    <select class="form-control selectpicker" data-live-search="true" id="selectorInsumo">
+                                        @foreach($insumos as $insumo)
+                                            <option value="{{$insumo->id}}">{{$insumo->nombre}}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+
+                            <div class="col-lg-2 col-md-12 col-sm-12 col-xs-12">
+                                <div class="form-group">
+
+                                    <input type="number" class="form-control" id="cantidad" min="1"  placeholder="Cantidad">
+                                </div>
+                            </div>
+
+                            <div class="col-lg-2 col-md-12 col-sm-12 col-xs-12">
+                                <div class="form-group">
+
+                                    <input type="number" class="form-control" id="precio" min="1" placeholder="P. Unitario">
+                                </div>
+                            </div>
+
+                            <div class="col-lg-2 col-md-12 col-sm-12 col-xs-12">
+                                <div class="form-group">
+
+                                    <button id="btn_agregar" type="button" onclick="agregar()"  class="btn btn-success btn-sm btn-block">
+                                         <i class="fa fa-plus fa-2x"></i>
+                                    </button>
                                 </div>
                             </div>
                         </div>
 
+                        <div class="table-responsive">
+                            <table class="table table-hover table-bordered color-table info-table">
+                                <thead>
+                                <tr>
+                                    <th class="text-right">OPC</th>
+                                    <th class="text-center w-50">INSUMO</th>
+                                    <th class="text-center">CANT</th>
+                                    <th class="text-center">P. UNITARIO</th>
+                                    <th class="text-center w-25">SUB-TOTAL</th>
+                                </tr>
+                                </thead>
+                                <tbody id="detalle">
+                                </tbody>
+                            </table>
 
-
-
-
-
-
-
+                        </div>
 
                         <a href="{{url('ingresos')}}" class="btn btn-warning">Atras</a>
                         <button type="submit" class="btn btn-info">Guardar</button>
@@ -75,4 +104,82 @@
             </div>
         </div>
     </div>
+    @push('arriba')
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.9/dist/css/bootstrap-select.min.css">
+    @endpush
+    @push('scripts')
+        <script>
+            $(document).ready(
+                function () {
+
+                }
+            );
+
+            var cont = 0;
+            var cantidad = [];
+            var precio = [];
+            var subTotal = [];
+
+            function agregar() {
+
+                idInsumo = $('#selectorInsumo').val();
+                nombreInsumo = $('#selectorInsumo option:selected').text();
+                cantidad[cont] = $('#cantidad').val();
+                precio[cont] = $('#precio').val();
+
+                if (idInsumo != "" && idInsumo > 0 && cantidad[cont] != ""
+                    && cantidad[cont] > 0 && precio[cont] != "" && precio[cont] > 0){
+
+                    subTotal[cont] = cantidad[cont] * precio[cont];
+
+
+                    var fila=
+                        '<tr id="fila'+cont+'">' +
+                        '<td>' +
+                        '<button type="button" class="btn btn-danger btn-sm" onclick="quitar('+cont+');">' +
+                        '<i class="fa fa-times" aria-hidden="true"></i>' +
+                        '</button>' +
+                        '</td>' +
+                        '<td>' +
+                        '   <input type="hidden" class"form-control "  name="idInsumoT[]" value="'+idInsumo+'">'
+                        +nombreInsumo+
+                        '</td>' +
+                        '<td>' +
+                        '<input type="hidden" class"form-control "  name="cantidadT[]" value="'+cantidad[cont]+'">'
+                        +cantidad[cont]+
+                        '</td>' +
+                        '<td>' +
+                        '<input type="hidden" class"form-control "  name="precioT[]" value="'+precio[cont]+'">'
+                        +precio[cont]+
+                        '</td>' +
+                        '<td>'
+                        +subTotal[cont]+
+                        '</td> ' +
+                        '</tr>';
+
+                    cont++;
+                    limpiar();
+
+                    $("#detalle").append(fila); // sirve para anhadir una fila a los detalles
+
+                }
+
+            }
+
+            function quitar(index){
+                cont--;
+                $("#fila" + index).remove();
+            }
+
+
+            function limpiar(){
+                $("#cantidad").val("");
+                $("#precio").val("");
+            }
+
+
+
+        </script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.9/dist/js/bootstrap-select.min.js"></script>
+    @endpush
 @endsection
