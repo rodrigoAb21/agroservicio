59fc063f2406a500b5844a0ed72d0ed43c34253a
rodrigoDesktop <rodrigo.abasto21@gmail.com>
2020-07-28 08:05:01 -0400

    Poblando

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,47 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        // Unidad de Medida
+        DB::table('unidad_medida')->insert([
+            'nombre' => 'Kg',
+        ]);
+        DB::table('unidad_medida')->insert([
+            'nombre' => 'g',
+        ]);
+        DB::table('unidad_medida')->insert([
+            'nombre' => 'mg',
+        ]);
+        DB::table('unidad_medida')->insert([
+            'nombre' => 'L',
+        ]);
+        DB::table('unidad_medida')->insert([
+            'nombre' => 'mL',
+        ]);
+
+
+        // Tipo Fitosanitario
+        DB::table('tipoFitosanitario')->insert([
+            'nombre' => 'Herbicida',
+        ]);
+        DB::table('tipoFitosanitario')->insert([
+            'nombre' => 'Insecticida',
+        ]);
+        DB::table('tipoFitosanitario')->insert([
+            'nombre' => 'Acaricida',
+        ]);
+        DB::table('tipoFitosanitario')->insert([
+            'nombre' => 'Bactericida',
+        ]);
+        DB::table('tipoFitosanitario')->insert([
+            'nombre' => 'Fungicida',
+        ]);
+
+
+        // Proveedor
+
+        factory(\App\Proveedor::class,30)->create();
+
+
+
     }
 }
